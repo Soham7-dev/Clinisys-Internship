1413395ea7120e9b58fc684c562b296966899f4a
Soham Das <Soham.Das@clinisys.com>
2023-01-23 16:57:17 +0530

    adding first directory

---
+++ Abstraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hello{
+
+    public class Abstraction{
+
+        abstract class Animal{
+
+            public abstract void sound();
+
+            public abstract void move();
+
+        }
+
+        class Dog : Animal{
+
+            public override void sound()
+            {
+                Console.WriteLine("Dog Barks");
+            }
+
+            public override void move()
+            {
+                Console.WriteLine("Dog runs");
+            }
+        }
+
+        public static void Main(string[] args){
+
+            Dog d = new Dog();
+
+            d.sound();
+            d.move();
+        }
+    }
+}
