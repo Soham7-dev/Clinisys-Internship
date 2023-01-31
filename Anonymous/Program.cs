30d3d6f57f66786660b8c85feb8d5d5dea0eff07
Soham Das <Soham.Das@clinisys.com>
2023-01-31 14:38:54 +0530

    Adding DynamicType and Anonymous

---
+++ Anonymous/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello{
+
+    class Anonymous
+    {
+        static void Main(string[] args)
+        {
+            var anonymous = new { Name = "Flash", TopSpeed = "299,792,458 m/s"};
+            var newAnonymous = anonymous with { TopSpeed = "Greater than Speed of Light"};
+
+            Console.WriteLine(anonymous.Name + " " + anonymous.TopSpeed + " " + newAnonymous.TopSpeed);
+        }
+    }
+}
---
+++ DynamicType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello{
+
+    class DynamicType
+    {
+        static void Main(string[] args)
+        {
+            dynamic a = "Lionel Messi";
+
+            Console.WriteLine(a);
+
+            a = 10;
+
+            Console.WriteLine(a);
+        }
+    }
+}
