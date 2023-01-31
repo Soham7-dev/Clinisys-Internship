562dd66080828958cabd7f7cd7c3983e4fd7cdac
Soham Das <Soham.Das@clinisys.com>
2023-01-31 15:43:20 +0530

    Adding Func and Action

---
+++ FuncLamda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hello{
+
+    class FuncLamda
+    {
+        static void Main(string[] args)
+        {
+            //Expression Lambda
+            Func<int, int, int> add = (int a, int b) => (a+b);
+
+            int c = add(10,20);
+
+            Console.WriteLine("Sum of two Numbers {0}", c);
+
+            //Statement Lambda
+            Action<int, int> add2 = (int a, int b) => {int c = a+b; Console.WriteLine("Sum of Two Numbers: {0}", c);};
+
+            add2(40,50);
+        }
+    }
+}
