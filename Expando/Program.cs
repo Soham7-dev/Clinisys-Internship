734ee5a29a832a3243e86b8432aedc1ba1dfe9d6
Soham Das <Soham.Das@clinisys.com>
2023-01-31 14:17:13 +0530

    Adding ExpandoObject

---
+++ Expando/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Hello{
+
+    class Expando
+    {
+        static void Main(string[] args)
+        {
+            dynamic exp = new ExpandoObject();
+
+            exp.fname = "Soham";
+            exp.lname = "Das";
+            exp.age = 21;
+            exp.print = exp.fname + " " + exp.lname + String.Format(" is {0} years Young", exp.age);
+
+            foreach(var property in (IDictionary<String, Object>)exp)
+            {
+                Console.WriteLine(property.Key + " : " + property.Value);
+            }
+        }
+    }
+}
