2a50fdb56b1a5bdeeb2ce68de5c11de272e07032
Soham Das <Soham.Das@clinisys.com>
2023-02-02 14:55:24 +0530

    adding Dict

---
+++ Dict/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hello{
+
+    class Dict
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            dict.Add("Batman", "Smartest");
+            dict.Add("SuperMan", "Strongest");
+            dict.Add("WonderWoman", "Oldest");
+            dict.Add("Flash", "Fastest");
+
+            foreach(string name in dict.Keys)
+            {
+                if(name.Equals("Flash"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+                Console.WriteLine("{0} is {1}", name, dict[name]);
+            }
+
+            Console.ResetColor();
+
+            Console.WriteLine("Enter a new Justice League Member: ");
+
+            string? newMember = Console.ReadLine();
+
+            Console.WriteLine("Enter his/her Characteristics: ");
+
+            string? charac = Console.ReadLine();
+
+            if(newMember == null || charac == null)
+            {
+                throw new Exception("You Entered Null Value!");
+            }
+            else
+            {
+                dict.Add(newMember, charac);
+            }
+
+            foreach(string name in dict.Keys)
+            {
+                if(name.Equals("Flash"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine("{0} is {1}", name, dict[name]);
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
