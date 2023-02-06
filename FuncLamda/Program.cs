dc72d72551b0ecb63fa4ff105254e00a90d08d9f
Soham Das <Soham.Das@clinisys.com>
2023-02-06 18:14:36 +0530

    Updating FuncLambda

--- FuncLamda/Program.cs
+++ FuncLamda/Program.cs
@@ -7,14 +7,14 @@ namespace Hello{
         static void Main(string[] args)
         {
             //Expression Lambda
-            Func<int, int, int> add = (int a, int b) => (a+b);
+            Func<int, int, int> add = (a,b) => (a+b);
 
             int c = add(10,20);
 
             Console.WriteLine("Sum of two Numbers {0}", c);
 
             //Statement Lambda
-            Action<int, int> add2 = (int a, int b) => {int c = a+b; Console.WriteLine("Sum of Two Numbers: {0}", c);};
+            Action<int, int> add2 = ( a, b) => {int c = a+b; Console.WriteLine("Sum of Two Numbers: {0}", c);};
 
             add2(40,50);
         }
