ff312ffb7ba8b772a30c9416ea4f3d3358470e28
Soham Das <Soham.Das@clinisys.com>
2023-02-09 09:53:04 +0530

    Changing it to ValueTuple

--- Tuple/Program.cs
+++ Tuple/Program.cs
@@ -5,12 +5,12 @@ namespace Hello{
 
     class Tuple
     {
-        public delegate Tuple<int, int, int> Square((int a, int b, int c) t);
+        public delegate ValueTuple<int, int, int> Square((int a, int b, int c) t);
         static void Main(string[] args)
         {
             Square sq = new Square(square);
 
-            Tuple<int, int, int> ans = sq.Invoke((3,4,5));
+            ValueTuple<int, int, int> ans = sq.Invoke((3,4,5));
 
             Func<(int, int, int), (int, int, int)> cube = (a) => (a.Item1*a.Item1*a.Item1, a.Item2*a.Item2*a.Item2, a.Item3*a.Item3*a.Item3);
 
@@ -19,11 +19,11 @@ namespace Hello{
             Console.WriteLine("Cube of Tuple : " + cube((3,4,5)));
         }
 
-        public static Tuple<int, int, int> square((int a, int b, int c) t)
+        public static ValueTuple<int, int, int> square((int a, int b, int c) t)
         {
-            int a = t.Item1, b = t.Item2, c = t.Item3;
+            int x = t.a, y = t.b, z = t.c;
 
-            return new Tuple<int, int, int>(a*a,b*b,c*c);
+            return new ValueTuple<int, int, int>(x*x, y*y, z*z);
         }
     }
 }
